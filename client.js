57d43ade2c8edc378e60d5d62cf41cefb9e2911e
drib <calumholt@hotmail.com>
2023-10-30 14:14:21 -0600

    created darkmode toggle

--- client.js
+++ client.js
@@ -1,12 +1,24 @@
-// -------------NAV------------------- //
+// -------------NAV-TOGGLE------------------ //
 
 // Creating variable for nav button
-const navButton = document.querySelector("#button")
+const navButton = document.querySelector("#button");
 // create variable for container of navigation
-const navContainer = document.querySelector(".nav-container")
+const navContainer = document.querySelector(".nav-container");
 
 // create event listener for button
 navButton.addEventListener("click", () => {
 // toggle visible class 
-navContainer.classList.toggle("visible")
+navContainer.classList.toggle("visible");
 });
+
+// -------------DARK-MODE-TOGGLE----------------- //
+
+// Create variable for toggle
+const darkButton = document.querySelector("#darkmode")
+// create variable for elements being triggered
+const body = document.querySelector("body")
+// Add event listener to toggle
+darkButton.addEventListener("click", () => {
+  // make elements being triggered have darkmode class
+body.classList.toggle("darkmode")
+})
